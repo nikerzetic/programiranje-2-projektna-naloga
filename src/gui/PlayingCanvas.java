a3939249b854ba7870d204e5827669b9ca65df62
Nik Erzetič <nik.erzetic@student.fmf.uni-lj.si>
2019-05-20 10:00:42 +0200

    Resolved null pointer exeption in line 71 of PlayingCanvas

--- src/gui/PlayingCanvas.java
+++ src/gui/PlayingCanvas.java
@@ -43,24 +43,24 @@ public class PlayingCanvas extends JPanel {
 		super.paintComponent(g);
 		
 		setBackground(backgroundColor);
-		
+
 		int N = Game.getSize() + 1;
 		int height = getHeight();
 		int width = getWidth();
 		int size = Math.min(height, width) / N;
 		int pad_x = (width - size * N) /2;
 		int pad_y = (height - size * N) /2;
-		
+
 		g.translate(pad_x, pad_y);
-		
+
 		g.setColor(edgeColor);
 		g2.setStroke(new BasicStroke(edgeWidth));
 		g.drawLine(0, 0, N * size, 0);
 		g.drawLine(0, N * size, N * size, N * size);
 		g.drawLine(0, 0, 0, N * size);
 		g.drawLine(N * size, 0, N * size, N * size);
-		
-		
+
+
 		g.setColor(lineColor);
 		g2.setStroke(new BasicStroke(lineWidth));
 		for (int i = 01; i <= 15; i++) {
@@ -68,24 +68,26 @@ public class PlayingCanvas extends JPanel {
 			g.drawLine(0, i * size, N * size, i * size);
 		}
 		
-		StoneColor[][] grid = master.getGame().getGrid();
-		
-		for (int i = 0; i < N - 1; i++) {
-			for (int j = 0; j < N - 1; j++) {
-				StoneColor stone = grid[i][j];
-				if (stone == StoneColor.EMPTY) {
-					continue;
+		if (master != null) {
+			StoneColor[][] grid = master.getGame().getGrid();
+
+			for (int i = 0; i < N - 1; i++) {
+				for (int j = 0; j < N - 1; j++) {
+					StoneColor stone = grid[i][j];
+					if (stone == StoneColor.EMPTY) {
+						continue;
+					}
+					else if (stone == StoneColor.BLACK) {
+						g.setColor(blackColor);
+					}
+					else if (stone == StoneColor.WHITE) {
+						g.setColor(whiteColor);
+					}
+					g.fillOval((i + 1) * size - stoneRadius, (j + 1) * size - stoneRadius, 2 * stoneRadius, 2 * stoneRadius);
 				}
-				else if (stone == StoneColor.BLACK) {
-					g.setColor(blackColor);
-				}
-				else if (stone == StoneColor.WHITE) {
-					g.setColor(whiteColor);
-				}
-				g.fillOval((i + 1) * size - stoneRadius, (j + 1) * size - stoneRadius, 2 * stoneRadius, 2 * stoneRadius);
 			}
 		}
-		
+
 	}
 
 }
